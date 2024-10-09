06aebd3b6217f9ef4ee818477e8d4311794d8449
Kiran Kumar Kotari <kirankotari@live.com>
2024-10-08 22:51:09 -0400

    toml updates

--- legallint/plugins/python/main.py
+++ legallint/plugins/python/main.py
@@ -3,6 +3,7 @@ LegalLint python locates 3rd party libraries used and returns name and metadata
 """
 
 from legallint.plugin import Plugin
+from legallint.utils import get_pwd, get_lines, get_matching_keys, read_toml
 
 
 class PythonPlugin(Plugin):
@@ -11,3 +12,84 @@ class PythonPlugin(Plugin):
 
     def run(self, data):
         return f"Processed Python data: {data}"
+
+
+class Toml:
+    basedir = get_pwd()
+    file = 'pyproject.toml'
+
+    @classmethod
+    def parse(cls, fpath=None):
+        config = {}
+        if not fpath:
+            fpath = f"{cls.basedir}/{cls.file}"
+
+        lines = get_lines(fpath)
+        for line in lines:
+            if not line or line.startswith('#'):
+                continue
+
+            # Handle section headers (e.g. [tool.poetry])
+            if line.startswith('[') and line.endswith(']'):
+                section = line[1:-1]
+                config[section] = {}
+
+            # Handle key-value pairs (e.g. version = "1.0.0")
+            elif '=' in line:
+                key, value = line.split('=', 1)
+                key = key.strip()
+                value = value.strip().strip('"')
+                if section:
+                    config[section][key] = value
+                else:
+                    config[key] = value
+        return config
+
+    @classmethod
+    def get_req(cls, fpath=None):
+        deps = {}
+        config = cls.parse(fpath)
+        if not len(config):
+            print("no dependencies found")
+            return
+        for each in get_matching_keys('dependencies', list(config.keys())):
+            key = each.split('poetry.')[-1]
+            if 'group' in key:
+                key = key.split('group.')[-1]
+            if 'python' in config[each]:
+                del config[each]['python']
+            deps[key] = config[each].keys()
+        print(deps)
+
+    @classmethod
+    def get_dependencies(cls, fpath=None):
+        if not fpath:
+            fpath = f"{cls.basedir}/{cls.file}"
+        config = read_toml(fpath)
+
+        dependencies = {}
+        # Poetry dependencies
+        if 'tool' in config and 'poetry' in config['tool']:
+            poetry = config['tool']['poetry']
+            for matched_key in get_matching_keys('dependencies', list(poetry.keys())):
+                if 'python' in poetry[matched_key]:
+                    del poetry[matched_key]['python']
+                dependencies[matched_key] = list(poetry[matched_key].keys())
+            if 'group' in poetry:
+                for group, group_deps in poetry['group'].items():
+                    if 'dependencies' in group_deps:
+                        dependencies[group] = list(group_deps['dependencies'].keys())
+
+        # Setuptools dependencies (if present)
+        if 'project' in config and 'dependencies' in config['project']:
+            dependencies['setuptools'] = [
+                each.split('>=')[0] if '>=' in each else each.split('==')[0] if '==' in each else each
+                for each in config['project']['dependencies']
+            ]
+        return dependencies
+
+
+
+if __name__ == "__main__":
+    print(Toml.get_dependencies())
+
--- legallint/utils.py
+++ legallint/utils.py
@@ -1,12 +1,18 @@
 import os
+import re
 import sys
 
+import json
+import toml
+
+
 def get_lines(fpath):
+    if not os.path.isfile(fpath):
+        print(f"Error: The file '{fpath}' was not found.")
+        return []
     try:
         with open(fpath, 'r') as file:
             return [line.strip() for line in file.readlines()]
-    except FileNotFoundError:
-        print(f"Error: The file '{fpath}' was not found.")
     except IOError:
         print(f"Error: An error occurred while reading the file '{fpath}'.")
     return []
@@ -16,3 +22,21 @@ def get_basedir():
 
 def check_subclass(subclass, cls):
     return False if subclass is cls else issubclass(subclass, cls)
+
+def get_pwd():
+    return os.getcwd()
+
+def get_matching_keys(substring, keys):
+    return [key for key in keys if re.search(substring, key)]
+
+def read_json(fpath):
+    with open(fpath, 'r') as f:
+        return json.load(f)
+
+def write_json(fpath, data):
+    with open(fpath, 'w') as f:
+        json.dump(data, f, indent=2, sort_keys=True)
+
+def read_toml(fpath):
+    with open(fpath, 'r') as f:
+        return toml.load(f)
